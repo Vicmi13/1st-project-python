ec3b28062755c10d8f18c10f7d63a94dc0dfcffc
VicDev <vicm_13@hotmail.com>
2018-03-23 14:19:21 -0600

    Se agrega tabla de movie y se hace migrate

---
+++ src/movies/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# En un modelo todos los campos son obligatorios, para que NO lo sean se ponen dos flags
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True) #  Optional
+
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    summary = models.TextField()
+    director_name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    image = models.URLField()
+    rating = models.FloatField()
+
+    created_at = models.DateTimeField(auto_now_add=True)  # Saves the date when the object is created
+    modified_at = models.DateTimeField(auto_now=True)  # Saves the date when the object is updated
