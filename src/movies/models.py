1b6133124322bb9cf80f54b1c187f27d3e350aab
VicDev <vicm_13@hotmail.com>
2018-04-02 15:43:27 -0500

    Registros de nuestros modelos en el admin de Django

--- src/movies/models.py
+++ src/movies/models.py
@@ -6,6 +6,10 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True) #  Optional
 
+    def __str__(self):# 0 parameters
+        return self.name
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=50)
     summary = models.TextField()
